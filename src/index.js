363caf6d6960eeb156eea136c40b5db99c6af856
Prasetya Aji <prasetya.aji@reservasi.com>
2018-08-09 14:29:23 +0700

    react starter

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import style from "./style/app.css";
+
+const Index = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
