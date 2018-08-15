a8d62f4f810e9cab673097db0730ee35957dbc6b
Prasetya Aji <prasetya.aji@reservasi.com>
2018-08-15 15:47:43 +0700

    redux initial setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, browserHistory, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Users from './components/Users';
 // import { Page404 } from './jsx/component/Page404';
 
+
 const App = () =>(
-    <Router history={browserHistory}>
-        <div>
-            <Switch>
-                <Route exact path={'/'} render={props => <Users {...props} />} />
-                {/* <Route component={Page404}/> */}
-            </Switch>
-        </div>
-    </Router>
+    <Provider store={ store }>
+        <Router history={browserHistory}>
+            <div>
+                <Switch>
+                    <Route exact path={'/'} render={props => <Users {...props} />} />
+                    {/* <Route component={Page404}/> */}
+                </Switch>
+            </div>
+        </Router>
+    </Provider>
 );
 
 ReactDOM.render(<App/>, document.getElementById("index"));
